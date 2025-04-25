2a6966bbff6ea095fccef7af6db22b23343a7136
Jan Koscisz <koscis.j@gmail.com>
2025-04-25 10:41:45 +0200

    cr;

--- programs/scope/src/oracles/redstone.rs
+++ programs/scope/src/oracles/redstone.rs
@@ -7,15 +7,16 @@ use crate::{utils::account_deserialize, warn, DatedPrice, Price, ScopeError};
 #[cfg(not(feature = "skip_price_validation"))]
 /// Price is kept in 0.2% or 0.05% deviation threshold, depending on the feed, all the time.
 /// At least one update per 30h will happen.
-const VALID_PRICE_LIFETIME: i64 = 30 * crate::utils::SECONDS_PER_HOUR;
+const VALID_PRICE_LIFETIME_MS: i64 = 30 * crate::utils::SECONDS_PER_HOUR * MS_PER_SECOND;
 
-const MS_PER_SECONDS: u64 = 1_000;
+const MS_PER_SECOND: i64 = 1_000;
 const RESERVED_BYTE_SIZE: usize = 64;
+const U256_BYTE_SIZE: usize = 32;
 
 #[account]
 struct PriceData {
-    pub feed_id: [u8; 32],
-    pub value: [u8; 32],
+    pub feed_id: [u8; U256_BYTE_SIZE],
+    pub value: [u8; U256_BYTE_SIZE],
     pub timestamp: u64,
     pub write_timestamp: Option<u64>,
     pub write_slot_number: u64,
@@ -23,8 +24,8 @@ struct PriceData {
     pub _reserved: [u8; RESERVED_BYTE_SIZE],
 }
 
-fn redstone_value_to_price(raw_be_value: [u8; 32], decimals: u8) -> Result<Price> {
-    let u64_start_index = 32 - size_of::<u64>();
+fn redstone_value_to_price(raw_be_value: [u8; U256_BYTE_SIZE], decimals: u8) -> Result<Price> {
+    let u64_start_index = U256_BYTE_SIZE - size_of::<u64>();
 
     if !raw_be_value.iter().take(u64_start_index).all(|&v| v == 0) {
         warn!("Price overflow u64");
@@ -50,14 +51,14 @@ fn check_price_lifetime(timestamp_ms: Option<u64>, clock: &Clock) -> Result<()> 
     };
 
     let ms_since_last_udpate =
-        (clock.unix_timestamp * MS_PER_SECONDS).checked_div(timestamp_ms as i64);
+        (clock.unix_timestamp * MS_PER_SECOND).checked_sub(timestamp_ms as i64);
 
     match ms_since_last_udpate {
-        Some(ms) if ms <= VALID_PRICE_LIFETIME => Ok(()),
+        Some(ms) if ms <= VALID_PRICE_LIFETIME_MS => Ok(()),
         _ => {
             warn!(
                 "RedStone price feed account data has not been refreshed for more than: {:?}ms",
-                VALID_PRICE_LIFETIME
+                VALID_PRICE_LIFETIME_MS
             );
             return Err(ScopeError::PriceNotValid.into());
         }
@@ -71,13 +72,18 @@ pub fn get_price(price_info: &AccountInfo, _clock: &Clock) -> Result<DatedPrice>
     #[cfg(not(feature = "skip_price_validation"))]
     check_price_lifetime(price_data.write_timestamp, _clock)?;
 
+    let unix_timestamp = match price_data.write_timestamp {
+        Some(wt) => wt / MS_PER_SECOND as u64,
+        None => {
+            warn!("No write timestamp");
+            return Err(ScopeError::PriceNotValid.into());
+        }
+    };
+
     Ok(DatedPrice {
         price,
+        unix_timestamp,
         last_updated_slot: price_data.write_slot_number,
-        unix_timestamp: price_data
-            .write_timestamp
-            .expect("Checked in `check_price_life_time`")
-            / MS_PER_SECONDS,
         generic_data: Default::default(),
     })
 }
